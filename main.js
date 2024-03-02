55768e1bc5332debccdfbcf45a1d956783778657
singhsimran13 <simran.guru.71@gmail.com>
2024-03-02 17:02:58 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3100;
+const fs = require("fs");
+const path = require("path");
+
+(() => {
+  const files = fs.readdirSync("./files");
+
+  let groupedFiles = {};
+  files.forEach((x) => {
+    const ext = x.split(".").pop();
+
+    if (!groupedFiles[ext]) {
+      groupedFiles[ext] = [];
+    }
+    groupedFiles[ext].push(x);
+  });
+
+  for (const ext in groupedFiles) {
+    const folderPath = path.join("./files", ext);
+    // console.log(folderPath,'    PATH')
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+    groupedFiles[ext].forEach((file) => {
+      const oldPath = path.join("./files", file);
+      const newPath = path.join(folderPath, file);
+      fs.renameSync(oldPath, newPath);
+    });
+  }
+
+//   console.log(groupedFiles, "   extension");
+//   console.log(files, "   files");
+})();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Testing</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to this ${port} port`);
+});
